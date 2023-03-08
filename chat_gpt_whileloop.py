81860314f93d0fcf7a5a25f6152ace2aec7ea911
solverbot <kamaljp@gmail.com>
2023-03-08 17:47:27 +0530

    updated the loop to include the history file

--- chat_gpt_whileloop.py
+++ chat_gpt_whileloop.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import json, openai
 import warnings
 import os
@@ -11,6 +10,7 @@ readKey.read_file(open('apidata.config'))
 
 openai_org = readKey['OPENAI']["ORG"]
 openai_key = readKey['OPENAI']['KEY'] 
+file_loc = readKey['OPENAI']['FILE']
 
 def extract_response(org,key,role_player,request_content):
     openai.organization = org
@@ -31,7 +31,12 @@ while True:
     get_prompt = input("Give the prompt here: ")
     content_output = extract_response(org=openai_org, key=openai_key, 
                  role_player=get_role, request_content=get_prompt)
+    write_data = {"prompt":get_prompt,"reply":content_output,
+                  'time_queried':str(datetime.datetime.now())}
     print(content_output,end='\n')
+    with open(file_loc,'a+') as hist:
+        json.dump(obj=write_data,fp=hist)
+        hist.write('\n')
 
     exit_q = input("Want to Exit? y / n : ")
     if exit_q == 'y' or exit_q == 'Y':
